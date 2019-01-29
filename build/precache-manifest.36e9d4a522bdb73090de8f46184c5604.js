97d06fcca6be64ffef0592b76d44f3fcaa8aa97b
Kelvin Masilamani <kelvinator@Kelvins-MacBook-Pro-104.local>
2019-01-28 21:13:18 -0500

    build 2 - css added

---
+++ build/precache-manifest.36e9d4a522bdb73090de8f46184c5604.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e4c7d27db363685fd9333857f2d83a5",
+    "url": "/static/media/kelvin.6e4c7d27.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a2f387afc99dcd2fb999",
+    "url": "/static/js/main.a2f387af.chunk.js"
+  },
+  {
+    "revision": "2718a44cde55acf80e7f",
+    "url": "/static/js/1.2718a44c.chunk.js"
+  },
+  {
+    "revision": "a2f387afc99dcd2fb999",
+    "url": "/static/css/main.24984fd5.chunk.css"
+  },
+  {
+    "revision": "ab75536d058fdbd82335b4427c94493e",
+    "url": "/index.html"
+  }
+];
